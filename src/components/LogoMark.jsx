55e3feb73271e177017cd5b3d6cfdd171787febf
brent-prog <brent@3psolutions.ca>
2026-06-28 02:18:48 -0400

    Use public logo asset instead of CSS-built logo

--- src/components/LogoMark.jsx
+++ src/components/LogoMark.jsx
@@ -1,21 +1,11 @@
 export default function LogoMark() {
   return (
     <div className="logo-mark" aria-label="TWO BALL DARTS logo">
-      <div className="logo-flights">
-        <span className="flight left"></span>
-        <span className="flight right"></span>
-      </div>
-      <div className="golf-ball"></div>
-      <div className="logo-arc"></div>
-      <div className="logo-banner">
-        <strong>TWO BALL</strong>
-        <span>DARTS</span>
-      </div>
-      <div className="logo-darts">
-        <i className="dart left"></i>
-        <i className="dart right"></i>
-      </div>
-      <div className="dartboard"><span></span></div>
+      <img
+        src="/two-ball-darts-logo.png"
+        alt="TWO BALL DARTS"
+        style={{ width: '100%', height: '100%', objectFit: 'contain', display: 'block' }}
+      />
     </div>
   );
 }
