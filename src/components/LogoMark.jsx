4584e131e5b1ef88895c15c2a91fbb5e14a3cbc5
brent-prog <brent@3psolutions.ca>
2026-06-28 00:34:51 -0400

    Add constructed logo component

---
+++ src/components/LogoMark.jsx
@@ -0,0 +1,21 @@
+export default function LogoMark() {
+  return (
+    <div className="logo-mark" aria-label="TWO BALL DARTS logo">
+      <div className="logo-flights">
+        <span className="flight left"></span>
+        <span className="flight right"></span>
+      </div>
+      <div className="golf-ball"></div>
+      <div className="logo-arc"></div>
+      <div className="logo-banner">
+        <strong>TWO BALL</strong>
+        <span>DARTS</span>
+      </div>
+      <div className="logo-darts">
+        <i className="dart left"></i>
+        <i className="dart right"></i>
+      </div>
+      <div className="dartboard"><span></span></div>
+    </div>
+  );
+}
