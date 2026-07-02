b046d0c1275de081d25af534d2df385bf109efe6
brent-prog <brent@3psolutions.ca>
2026-07-02 14:04:42 -0400

    Add mobile app metadata

--- src/app/layout.jsx
+++ src/app/layout.jsx
@@ -4,12 +4,27 @@ import './rule-result-polish.css';
 
 export const metadata = {
   title: 'TWO BALL DARTS',
-  description: 'No gimmes. Just throw. Live golf-style darts scoring and official rules.'
+  applicationName: 'TwoBall Darts',
+  description: 'No gimmes. Just throw. Live golf-style darts scoring and official rules.',
+  manifest: '/manifest.json',
+  appleWebApp: {
+    capable: true,
+    title: 'TwoBall Darts',
+    statusBarStyle: 'black-translucent'
+  },
+  formatDetection: {
+    telephone: false
+  },
+  icons: {
+    icon: '/favicon.svg',
+    apple: '/app-icon.svg'
+  }
 };
 
 export const viewport = {
   width: 'device-width',
   initialScale: 1,
+  viewportFit: 'cover',
   themeColor: '#02140f'
 };
 
