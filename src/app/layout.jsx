40a02890ad8fbd7c798b114965a57254a1b3c1cb
brent-prog <brent@3psolutions.ca>
2026-08-19 18:56:03 -0400

    Load mobile leader summary cleanup

--- src/app/layout.jsx
+++ src/app/layout.jsx
@@ -5,6 +5,7 @@ import './player-row-overrides.css';
 import './honours-position-fix.css';
 import './hole-spin-animation.css';
 import './next-hole-nav.css';
+import './mobile-leader-summary.css';
 import RoundFlowEnhancer from '@/components/RoundFlowEnhancer';
 import StructuralCleanupEnhancer from '@/components/StructuralCleanupEnhancer';
 import HoleTransitionEnhancer from '@/components/HoleTransitionEnhancer';
