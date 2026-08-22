b1eeb00b1c76e800b33206c9f776e25df2de38b2
brent-prog <brent@3psolutions.ca>
2026-08-21 22:43:05 -0400

    Load score tile cleanup styles

--- src/app/layout.jsx
+++ src/app/layout.jsx
@@ -6,6 +6,7 @@ import './honours-position-fix.css';
 import './hole-spin-animation.css';
 import './score-action-button-states.css';
 import './scoring-mode-react-polish.css';
+import './score-tile-cleanup.css';
 import './app-icon-polish.css';
 import './live-scorecard-fixes.css';
 import FreshOpenTopGuard from '@/components/FreshOpenTopGuard';
