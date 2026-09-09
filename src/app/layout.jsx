7c5a09fe585d390409517648974384ce7a60c66c
brent-prog <brent@3psolutions.ca>
2026-09-09 16:59:19 -0400

    Wire score interaction rebrand

--- src/app/layout.jsx
+++ src/app/layout.jsx
@@ -15,6 +15,7 @@ import './home-actions-modern.css';
 import './final-scoring-overrides.css';
 import './twoball-rebrand.css';
 import './scoring-rebrand-preview.css';
+import './score-interactions-rebrand.css';
 import FreshOpenTopGuard from '@/components/FreshOpenTopGuard';
 import PersistentLeaderBadgeEnhancer from '@/components/PersistentLeaderBadgeEnhancer';
 import RoundCompletionSaveEnhancer from '@/components/RoundCompletionSaveEnhancer';
