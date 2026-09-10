d8a91ab321b90aa83b3d4cfb87d674bbf261aa70
brent-prog <brent@3psolutions.ca>
2026-09-10 02:41:17 -0400

    Load home live round removal

--- src/app/layout.jsx
+++ src/app/layout.jsx
@@ -23,6 +23,7 @@ import './player-result-colours.css';
 import './scored-row-action-final.css';
 import './round-complete-rebrand.css';
 import './live-scorecard-rebrand.css';
+import './home-live-round-hide.css';
 import FreshOpenTopGuard from '@/components/FreshOpenTopGuard';
 import PersistentLeaderBadgeEnhancer from '@/components/PersistentLeaderBadgeEnhancer';
 import RoundCompletionSaveEnhancer from '@/components/RoundCompletionSaveEnhancer';
