2854503d0765a0d24edb3b6a3e7548c7b956aa68
brent-prog <brent@3psolutions.ca>
2026-09-09 23:10:51 -0400

    Remove unstable scoring label observer

--- src/app/layout.jsx
+++ src/app/layout.jsx
@@ -19,7 +19,6 @@ import './score-interactions-polish.css';
 import './player-rows-rebrand.css';
 import './player-profile-selection-polish.css';
 import './empty-player-final.css';
-import './player-result-colours.css';
 import FreshOpenTopGuard from '@/components/FreshOpenTopGuard';
 import PersistentLeaderBadgeEnhancer from '@/components/PersistentLeaderBadgeEnhancer';
 import RoundCompletionSaveEnhancer from '@/components/RoundCompletionSaveEnhancer';
@@ -41,7 +40,6 @@ import RoundHistoryCards from '@/components/RoundHistoryCards';
 import MissingHoleGuardEnhancer from '@/components/MissingHoleGuardEnhancer';
 import LiveScorecardSyncEnhancer from '@/components/LiveScorecardSyncEnhancer';
 import ScoringFeedbackEnhancer from '@/components/ScoringFeedbackEnhancer';
-import ScoringActionLabelEnhancer from '@/components/ScoringActionLabelEnhancer';
 import OffBoardDartOptionEnhancer from '@/components/OffBoardDartOptionEnhancer';
 
 export const metadata = {
@@ -85,7 +83,6 @@ export default function RootLayout({ children }) {
         <MissingHoleGuardEnhancer />
         <LiveScorecardSyncEnhancer />
         <ScoringFeedbackEnhancer />
-        <ScoringActionLabelEnhancer />
         <OffBoardDartOptionEnhancer />
         {children}
       </body>
